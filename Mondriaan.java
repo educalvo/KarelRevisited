ecc6bd96591f7122672bf735943ac42a419c5b73
educalvo <educalvo99@gmail.com>
2017-09-17 20:00:16 +0200

    Added files

---
+++ Mondriaan.java
@@ -0,0 +1,12 @@
+package nl.ru.ai.p4ai.exercise2;
+
+import static nl.ru.ai.karel.Karel.*;
+
+public class Mondriaan
+{
+  public static void main(String[] args)
+  {
+    speed(100);
+     // your code here 
+  }
+}
